4be5b0f7d548f9a44d85ad44eb1282f21b90c8c6
Ann Locke <alocke42@gmail.com>
2025-11-14 15:01:19 -0500

    added flask app

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+    # Extract from the response
+    response_display = ("For the given statement, the system response is "
+     + "'anger': " + str(response['anger'])
+     + " ', disgust': " + str(response['disgust'])
+     + " ', fear': " + str(response['fear'])
+     + " ', joy': " + str(response['joy'])
+     + " and 'sadness': " + str(response['sadness'])
+     + ". The dominant emotion is " + str(response['dominant_emotion']) + "."
+    )
+    # Return a formatted string
+    return response_display
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
